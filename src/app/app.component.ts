3c0e5eb851f93b1252eda442088b5a3d19dd11c8
Hanyu Xiao <hanyu.xiao@gmail.com>
2020-09-05 20:36:34 -0700

    Starter project for Angular + TensorFlow.js

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-tfjs-starter';
+  tfVersion = tf.version.tfjs;
+}
